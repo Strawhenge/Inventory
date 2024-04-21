6d495e45f7e538956925883633169898f8d34fb1
Matt Bristow <strawhengestudios@gmail.com>
2024-04-21 22:24:36 +0100

    refactor, added interface for apparel data

--- Strawhenge.Inventory.Unity/Assets/Package/Runtime/Apparel/ApparelPieceScriptableObject.cs
+++ Strawhenge.Inventory.Unity/Assets/Package/Runtime/Apparel/ApparelPieceScriptableObject.cs
@@ -3,13 +3,15 @@
 namespace Strawhenge.Inventory.Unity.Apparel
 {
     [CreateAssetMenu(menuName = "Strawhenge/Inventory/Apparel/ApparelPiece")]
-    public class ApparelPieceScriptableObject : ScriptableObject
+    public class ApparelPieceScriptableObject : ScriptableObject, IApparelPieceData
     {
         [SerializeField] GameObject _prefab;
         [SerializeField] ApparelSlotScriptableObject _slot;
         [SerializeField] Vector3 _position;
         [SerializeField] Vector3 _rotation;
         [SerializeField] Vector3 _scale = new Vector3(1, 1, 1);
+
+        public string Name => name;
 
         public GameObject Prefab => _prefab;
 
---
+++ Strawhenge.Inventory.Unity/Assets/Package/Runtime/Apparel/IApparelPieceData.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Strawhenge.Inventory.Unity.Apparel
+{
+    public interface IApparelPieceData
+    {
+        string Name { get; }
+
+        GameObject Prefab { get; }
+
+        string Slot { get; }
+
+        Vector3 Position { get; }
+
+        Quaternion Rotation { get; }
+
+        Vector3 Scale { get; }
+    }
+}
