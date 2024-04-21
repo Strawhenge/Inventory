73d83308a4ba86c0c65b52d700a4058ef22de37b
Matt Bristow <strawhengestudios@gmail.com>
2024-04-21 22:39:06 +0100

    implemented apparel repo and refactored item repo

--- Strawhenge.Inventory.Unity/Assets/Package/Runtime/Data/ItemRepository.cs
+++ Strawhenge.Inventory.Unity/Assets/Package/Runtime/Data/ItemRepository.cs
@@ -11,19 +11,15 @@ namespace Strawhenge.Inventory.Unity.Data
 
         public ItemRepository(ISettings settings)
         {
-            _scriptableObjects = Resources.LoadAll<ItemScriptableObject>(path: settings.ItemScriptableObjectsPath)
+            _scriptableObjects = Resources
+                .LoadAll<ItemScriptableObject>(path: settings.ItemScriptableObjectsPath)
                 .ToArray<IItemData>();
         }
 
         public Maybe<IItemData> FindByName(string name)
         {
-            var first = _scriptableObjects.FirstOrDefault(x =>
+            return _scriptableObjects.FirstOrNone(x =>
                 x.Name.Equals(name, System.StringComparison.OrdinalIgnoreCase));
-
-            if (first == null)
-                return Maybe.None<IItemData>();
-
-            return Maybe.Some(first);
         }
     }
 }
