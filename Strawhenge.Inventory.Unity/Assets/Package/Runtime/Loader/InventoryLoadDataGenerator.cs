c9758ba0621c004bf03f1d0b843335ac00b6b2d1
Matt Bristow <strawhengestudios@gmail.com>
2024-04-24 20:41:14 +0100

    added initial load data generator

---
+++ Strawhenge.Inventory.Unity/Assets/Package/Runtime/Loader/InventoryLoadDataGenerator.cs
@@ -0,0 +1,33 @@
+﻿using Strawhenge.Inventory.Unity.Apparel;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Strawhenge.Inventory.Unity.Loader
+{
+    public class InventoryLoadDataGenerator
+    {
+        readonly ApparelManager _apparelManager;
+
+        public InventoryLoadDataGenerator(ApparelManager apparelManager)
+        {
+            _apparelManager = apparelManager;
+        }
+
+        public InventoryLoadData GenerateCurrentLoadData()
+        {
+            return new InventoryLoadData
+            {
+                EquippedApparel = GetEquippedApparelNames().ToArray()
+            };
+        }
+
+        IEnumerable<string> GetEquippedApparelNames()
+        {
+            foreach (var slot in _apparelManager.Slots)
+            {
+                if (slot.CurrentPiece.HasSome(out var apparelPiece))
+                    yield return apparelPiece.Name;
+            }
+        }
+    }
+}
