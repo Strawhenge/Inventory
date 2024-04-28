0d076cfee03f345ec1f75f8bef667313fb0ace0d
Matt Bristow <strawhengestudios@gmail.com>
2024-04-28 16:54:09 +0100

    tidy up

---
+++ Strawhenge.Inventory.Unity/Assets/Package/Runtime/Menus/HandsMenuScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Strawhenge.Inventory.Unity
+{
+    public class HandsMenuScript : MonoBehaviour
+    {
+        [SerializeField] ItemInHandMenuEntryScript _leftHand;
+        [SerializeField] ItemInHandMenuEntryScript _rightHand;
+    }
+}
