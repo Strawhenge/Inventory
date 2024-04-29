b78678f565c6c7b084f3a16863065df193a51ef4
Matt Bristow <strawhengestudios@gmail.com>
2024-04-29 23:50:24 +0100

    added swap hands extention

---
+++ Strawhenge.Inventory/Strawhenge.Inventory/InventoryExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Strawhenge.Inventory
+{
+    public static class InventoryExtensions
+    {
+        public static void SwapHands(this IInventory inventory)
+        {
+            var leftHandItem = inventory.LeftHand.CurrentItem;
+            inventory.RightHand.CurrentItem.Do(item => item.HoldLeftHand());
+            leftHandItem.Do(item => item.HoldRightHand());
+        }
+    }
+}
