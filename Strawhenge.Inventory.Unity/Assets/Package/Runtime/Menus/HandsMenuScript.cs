8c9d11a4e0e268a466854934f450ab4daed867d0
Matt Bristow <strawhengestudios@gmail.com>
2024-04-29 14:50:25 +0100

    added swap hands button

--- Strawhenge.Inventory.Unity/Assets/Package/Runtime/Menus/HandsMenuScript.cs
+++ Strawhenge.Inventory.Unity/Assets/Package/Runtime/Menus/HandsMenuScript.cs
@@ -1,17 +1,42 @@
 using Strawhenge.Inventory.Containers;
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Strawhenge.Inventory.Unity
 {
     public class HandsMenuScript : MonoBehaviour
     {
-        [SerializeField] ItemInHandMenuEntryScript _leftHand;
-        [SerializeField] ItemInHandMenuEntryScript _rightHand;
+        [SerializeField] ItemInHandMenuEntryScript _leftHandMenu;
+        [SerializeField] ItemInHandMenuEntryScript _rightHandMenu;
+        [SerializeField] Button _swapHandButton;
+
+        IItemContainer _leftHand;
+        IItemContainer _rightHand;
+
+        void Awake()
+        {
+            _swapHandButton.onClick.AddListener(OnSwapHandButton);
+        }
 
         internal void Set(IItemContainer leftHand, IItemContainer rightHand)
         {
-            _leftHand.Set(leftHand);
-            _rightHand.Set(rightHand);
+            _leftHand = leftHand;
+            _leftHandMenu.Set(leftHand);
+
+            _rightHand = rightHand;
+            _rightHandMenu.Set(rightHand);
+        }
+
+        void OnSwapHandButton()
+        {
+            if (_leftHand == null || _rightHand == null)
+                return;
+
+            var rightHandItem = _rightHand.CurrentItem;
+
+            _leftHand.CurrentItem.Do(x => x.HoldRightHand());
+            rightHandItem.Do(x => x.HoldLeftHand());
         }
     }
 }
