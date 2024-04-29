1173979402d1c68b3bf739829fc7036be15ba88a
Matt Bristow <strawhengestudios@gmail.com>
2024-04-29 17:54:46 +0100

    added menu show and hide function

--- Strawhenge.Inventory.Unity/Assets/Package/Runtime/Menus/InventoryMenuScript.cs
+++ Strawhenge.Inventory.Unity/Assets/Package/Runtime/Menus/InventoryMenuScript.cs
@@ -9,6 +9,7 @@ namespace Strawhenge.Inventory.Unity
 {
     public class InventoryMenuScript : MonoBehaviour
     {
+        [SerializeField] Canvas _canvas;
         [SerializeField] ApparelSlotsMenuScript _apparelSlotsMenu;
         [SerializeField] HandsMenuScript _handsMenu;
         [SerializeField] HolstersMenuScript _holstersMenu;
@@ -23,9 +24,23 @@ namespace Strawhenge.Inventory.Unity
         {
             yield return new WaitUntil(() => _inventoryScript.IsConfigurationComplete);
 
+            yield return new WaitUntil(() => _canvas.enabled);
+
             _apparelSlotsMenu.Set(_inventoryScript.Inventory.ApparelSlots);
             _holstersMenu.Set(_inventoryScript.Inventory.Holsters);
             _handsMenu.Set(_inventoryScript.Inventory.LeftHand, _inventoryScript.Inventory.RightHand);
+        }
+
+        [ContextMenu(nameof(Show))]
+        public void Show()
+        {
+            _canvas.enabled = true;
+        }
+
+        [ContextMenu(nameof(Hide))]
+        public void Hide()
+        {
+            _canvas.enabled = false;
         }
     }
 }
