9f96096427cf922599679a5c48fe75ef4285f0f2
Matt Bristow <strawhengestudios@gmail.com>
2024-05-02 19:54:39 +0100

    added initial inventory item container script

---
+++ Strawhenge.Inventory.Unity/Assets/Package/Runtime/ItemContainer/InventoryItemContainerScript.cs
@@ -0,0 +1,13 @@
+﻿using Strawhenge.Inventory.Unity.Apparel;
+using Strawhenge.Inventory.Unity.Data;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Strawhenge.Inventory.Unity
+{
+    public class InventoryItemContainerScript : MonoBehaviour
+    {
+        public IItemContainerSource Source => throw new NotImplementedException();
+    }
+}
