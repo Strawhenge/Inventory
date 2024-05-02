16bc0849e6f476b61da32cb3f24e807afffb4775
Matt Bristow <strawhengestudios@gmail.com>
2024-05-02 16:33:28 +0100

    initial structure for apparel in container menu

---
+++ Strawhenge.Inventory.Unity/Assets/Package/Runtime/Menu/ApparelPieceMenuEntryScript.cs
@@ -0,0 +1,45 @@
+using Strawhenge.Inventory.Unity.Apparel;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Strawhenge.Inventory.Unity
+{
+    public class ApparelPieceMenuEntryScript : MonoBehaviour
+    {
+        [SerializeField] Text _slotNameText;
+        [SerializeField] Text _itemNameText;
+        [SerializeField] Button _equipButton;
+
+        IInventory _inventory;
+        IContainedItem<IApparelPieceData> _containedItem;
+
+        void Awake()
+        {
+            _equipButton.onClick.AddListener(OnEquipButton);
+        }
+
+        public void Set(IInventory inventory, IContainedItem<IApparelPieceData> containedItem)
+        {
+            _inventory = inventory;
+            _containedItem = containedItem;
+
+            _slotNameText.text = containedItem.Item.Slot;
+            _itemNameText.text = containedItem.Item.Name;
+        }
+
+        void OnEquipButton()
+        {
+            if (_inventory == null || _containedItem == null)
+                return;
+
+            _inventory
+                .CreateApparelPiece(_containedItem.Item)
+                .Equip();
+
+            _containedItem.RemoveFromContainer();
+        }
+    }
+}
--- Strawhenge.Inventory.Unity/Assets/Package/Runtime/Menu/ItemContainerMenuScript.cs
+++ Strawhenge.Inventory.Unity/Assets/Package/Runtime/Menu/ItemContainerMenuScript.cs
@@ -1,3 +1,5 @@
+using Strawhenge.Inventory.Unity.Apparel;
+using Strawhenge.Inventory.Unity.Monobehaviours;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +8,26 @@ namespace Strawhenge.Inventory.Unity
 {
     public class ItemContainerMenuScript : MonoBehaviour
     {
-       
+        [SerializeField] InventoryScript _inventoryScript;
+
+        public void Open(IItemContainerSource source)
+        {
+        }
+
+        public void Close()
+        {
+        }
+    }
+
+    public interface IItemContainerSource
+    {
+        IReadOnlyList<IContainedItem<IApparelPieceData>> ApparelPieces { get; }
+    }
+
+    public interface IContainedItem<T>
+    {
+        T Item { get; }
+
+        void RemoveFromContainer();
     }
 }
