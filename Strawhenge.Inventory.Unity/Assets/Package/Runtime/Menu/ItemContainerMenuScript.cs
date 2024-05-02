1d6b00da8a62da653b087d17c1f5c922535577bd
Matt Bristow <strawhengestudios@gmail.com>
2024-05-02 17:02:54 +0100

    implemented apparle menu entry instantiation

--- Strawhenge.Inventory.Unity/Assets/Package/Runtime/Menu/ItemContainerMenuScript.cs
+++ Strawhenge.Inventory.Unity/Assets/Package/Runtime/Menu/ItemContainerMenuScript.cs
@@ -8,14 +8,32 @@ namespace Strawhenge.Inventory.Unity
 {
     public class ItemContainerMenuScript : MonoBehaviour
     {
+        [SerializeField] Canvas _canvas;
+        [SerializeField] RectTransform _entriesContainer;
+        [SerializeField] ApparelPieceMenuEntryScript _apparelPieceMenuEntryPrefab;
         [SerializeField] InventoryScript _inventoryScript;
+
+        readonly List<GameObject> _menuEntries = new List<GameObject>();
 
         public void Open(IItemContainerSource source)
         {
+            foreach (var apparelPiece in source.ApparelPieces)
+            {
+                var menuEntry = Instantiate(_apparelPieceMenuEntryPrefab, parent: _entriesContainer);
+                menuEntry.Set(_inventoryScript.Inventory, apparelPiece);
+                _menuEntries.Add(menuEntry.gameObject);
+            }
+
+            _canvas.enabled = true;
         }
 
         public void Close()
         {
+            foreach (var gameObject in _menuEntries)
+                Destroy(gameObject);
+
+            _menuEntries.Clear();
+            _canvas.enabled = false;
         }
     }
 
