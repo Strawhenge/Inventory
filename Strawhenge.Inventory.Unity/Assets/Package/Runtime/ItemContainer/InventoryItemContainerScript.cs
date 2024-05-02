995dcaf491c4f6a595cda87007c83a27cfa9ac4f
Matt Bristow <strawhengestudios@gmail.com>
2024-05-02 23:13:11 +0100

    implemented inventory container source

--- Strawhenge.Inventory.Unity/Assets/Package/Runtime/ItemContainer/InventoryItemContainerScript.cs
+++ Strawhenge.Inventory.Unity/Assets/Package/Runtime/ItemContainer/InventoryItemContainerScript.cs
@@ -1,13 +1,23 @@
 ﻿using Strawhenge.Inventory.Unity.Apparel;
 using Strawhenge.Inventory.Unity.Data;
 using System;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace Strawhenge.Inventory.Unity
 {
     public class InventoryItemContainerScript : MonoBehaviour
     {
-        public IItemContainerSource Source => throw new NotImplementedException();
+        public IInventory Inventory { private get; set; }
+
+        public IItemRepository ItemRepository { private get; set; }
+
+        public IApparelRepository ApparelRepository { private get; set; }
+
+        public IItemContainerSource Source { get; private set; }
+
+        void Start()
+        {
+            Source = new InventoryItemContainerSource(Inventory, ItemRepository, ApparelRepository);
+        }
     }
 }
---
+++ Strawhenge.Inventory.Unity/Assets/Package/Runtime/ItemContainer/InventoryItemContainerSource.cs
@@ -0,0 +1,62 @@
+﻿using Strawhenge.Inventory.Items;
+using Strawhenge.Inventory.Unity.Apparel;
+using Strawhenge.Inventory.Unity.Data;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Strawhenge.Inventory.Unity
+{
+    public class InventoryItemContainerSource : IItemContainerSource
+    {
+        readonly IInventory _inventory;
+        readonly IItemRepository _itemRepository;
+        readonly IApparelRepository _apparelRepository;
+
+        public InventoryItemContainerSource(
+            IInventory inventory,
+            IItemRepository itemRepository,
+            IApparelRepository apparelRepository)
+        {
+            _inventory = inventory;
+            _itemRepository = itemRepository;
+            _apparelRepository = apparelRepository;
+        }
+
+        public IReadOnlyList<IContainedItem<IItemData>> GetItems()
+        {
+            return _inventory.LeftHand.CurrentItem.AsEnumerable()
+                .Concat(_inventory.RightHand.CurrentItem.AsEnumerable())
+                .Concat(_inventory.Holsters.SelectMany(x => x.CurrentItem.AsEnumerable()))
+                .Concat(_inventory.StoredItems)
+                .Distinct()
+                .SelectMany(item =>
+                {
+                    return _itemRepository
+                        .FindByName(item.Name)
+                        .Map(data => new ContainedItem<IItemData>(data, () =>
+                        {
+                            item.ClearFromHandsPreference = ClearFromHandsPreference.Disappear;
+                            item.ClearFromHolsterPreference = ClearFromHolsterPreference.Disappear;
+                            item.ClearFromHands();
+                            item.UnequipFromHolster();
+                        }))
+                        .AsEnumerable();
+                })
+                .ToArray();
+        }
+
+        public IReadOnlyList<IContainedItem<IApparelPieceData>> GetApparelPieces()
+        {
+            return _inventory.ApparelSlots
+                .SelectMany(x => x.CurrentPiece.AsEnumerable())
+                .SelectMany(apparelPiece =>
+                {
+                    return _apparelRepository
+                        .FindByName(apparelPiece.Name)
+                        .Map(data => new ContainedItem<IApparelPieceData>(data, apparelPiece.Unequip))
+                        .AsEnumerable();
+                })
+                .ToArray();
+        }
+    }
+}
