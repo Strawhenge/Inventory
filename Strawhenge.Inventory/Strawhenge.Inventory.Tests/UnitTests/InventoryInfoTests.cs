f4c36ca5719800a05192b2fd1f6d3b5cfc0201ad
Matt Bristow <strawhengestudios@gmail.com>
2024-05-24 19:40:58 +0100

    added test for inventory info

--- Strawhenge.Inventory/Strawhenge.Inventory.Tests/UnitTests/InventoryInfoTests.cs
+++ Strawhenge.Inventory/Strawhenge.Inventory.Tests/UnitTests/InventoryInfoTests.cs
@@ -14,6 +14,11 @@ namespace Strawhenge.Inventory.Tests.UnitTests
         const string TorsoApparelSlot = "Torso";
         const string HipHolster = "Hip";
         const string BackHolster = "Back";
+        const string Hat = "Hat";
+        const string BodyArmour = "Body Armour";
+        const string Hammer = "Hammer";
+        const string Stick = "Stick";
+        const string Crowbar = "Crowbar";
 
         readonly TestContext _context;
         readonly InventoryInfoGenerator _infoGenerator;
@@ -41,42 +46,36 @@ namespace Strawhenge.Inventory.Tests.UnitTests
         [Fact]
         public void Info_should_contain_equipped_apparel()
         {
-            const string hat = "Hat";
-            const string bodyArmour = "Body Armour";
-
-            _context.CreateApparelPiece(hat, HeadApparelSlot)
+            _context.CreateApparelPiece(Hat, HeadApparelSlot)
                 .Equip();
-            _context.CreateApparelPiece(bodyArmour, TorsoApparelSlot)
+            _context.CreateApparelPiece(BodyArmour, TorsoApparelSlot)
                 .Equip();
 
             var info = _infoGenerator.GenerateCurrentInfo();
 
             Assert.Equal(2, info.EquippedApparel.Count);
 
-            Assert.Contains(hat, info.EquippedApparel);
-            Assert.Contains(bodyArmour, info.EquippedApparel);
+            Assert.Contains(Hat, info.EquippedApparel);
+            Assert.Contains(BodyArmour, info.EquippedApparel);
         }
 
         [Fact]
         public void Info_should_contain_stored_items()
         {
-            const string hammer = "Hammer";
-            const string stick = "Stick";
-
             _context.ItemStorage.Add(
-                _context.CreateItem(hammer, ItemSize.OneHanded));
+                _context.CreateItem(Hammer, ItemSize.OneHanded));
             _context.ItemStorage.Add(
-                _context.CreateItem(stick, ItemSize.TwoHanded));
+                _context.CreateItem(Stick, ItemSize.TwoHanded));
 
             var info = _infoGenerator.GenerateCurrentInfo();
 
             Assert.Equal(2, info.Items.Count);
 
-            var hammerInfo = Assert.Single(info.Items, x => x.ItemName == hammer);
+            var hammerInfo = Assert.Single(info.Items, x => x.ItemName == Hammer);
             Assert.True(hammerInfo.IsInStorage);
             Assert.Empty(hammerInfo.HolsterName);
 
-            var stickInfo = Assert.Single(info.Items, x => x.ItemName == stick);
+            var stickInfo = Assert.Single(info.Items, x => x.ItemName == Stick);
             Assert.True(stickInfo.IsInStorage);
             Assert.Empty(stickInfo.HolsterName);
         }
@@ -84,21 +83,18 @@ namespace Strawhenge.Inventory.Tests.UnitTests
         [Fact]
         public void Info_should_contain_holstered_items()
         {
-            const string hammer = "Hammer";
-            const string stick = "Stick";
-
-            _context.CreateItem(hammer, ItemSize.OneHanded, HipHolster).Holsters.First().Equip();
-            _context.CreateItem(stick, ItemSize.OneHanded, BackHolster).Holsters.First().Equip();
+            _context.CreateItem(Hammer, ItemSize.OneHanded, HipHolster).Holsters.First().Equip();
+            _context.CreateItem(Stick, ItemSize.OneHanded, BackHolster).Holsters.First().Equip();
 
             var info = _infoGenerator.GenerateCurrentInfo();
 
             Assert.Equal(2, info.Items.Count);
 
-            var hammerInfo = Assert.Single(info.Items, x => x.ItemName == hammer);
+            var hammerInfo = Assert.Single(info.Items, x => x.ItemName == Hammer);
             Assert.Equal(HipHolster, hammerInfo.HolsterName);
             Assert.False(hammerInfo.IsInStorage);
 
-            var stickInfo = Assert.Single(info.Items, x => x.ItemName == stick);
+            var stickInfo = Assert.Single(info.Items, x => x.ItemName == Stick);
             Assert.Equal(BackHolster, stickInfo.HolsterName);
             Assert.False(stickInfo.IsInStorage);
         }
@@ -106,27 +102,62 @@ namespace Strawhenge.Inventory.Tests.UnitTests
         [Fact]
         public void Info_should_contain_held_items()
         {
-            const string hammer = "Hammer";
-            const string stick = "Stick";
-
-            _context.CreateItem(hammer, ItemSize.OneHanded, HipHolster).HoldLeftHand();
-            _context.CreateItem(stick, ItemSize.OneHanded, BackHolster).HoldRightHand();
+            _context.CreateItem(Hammer, ItemSize.OneHanded, HipHolster).HoldLeftHand();
+            _context.CreateItem(Stick, ItemSize.OneHanded, BackHolster).HoldRightHand();
 
             var info = _infoGenerator.GenerateCurrentInfo();
 
             Assert.Equal(2, info.Items.Count);
 
-            var hammerInfo = Assert.Single(info.Items, x => x.ItemName == hammer);
+            var hammerInfo = Assert.Single(info.Items, x => x.ItemName == Hammer);
             Assert.True(hammerInfo.IsInLeftHand);
             Assert.False(hammerInfo.IsInRightHand);
             Assert.Empty(hammerInfo.HolsterName);
             Assert.False(hammerInfo.IsInStorage);
 
-            var stickInfo = Assert.Single(info.Items, x => x.ItemName == stick);
+            var stickInfo = Assert.Single(info.Items, x => x.ItemName == Stick);
             Assert.True(stickInfo.IsInRightHand);
             Assert.False(stickInfo.IsInLeftHand);
             Assert.Empty(stickInfo.HolsterName);
             Assert.False(stickInfo.IsInStorage);
+        }
+
+        [Fact]
+        public void Info_should_not_contain_duplicate_items_that_are_both_in_hand_holster_or_storage()
+        {
+            var hammer = _context.CreateItem(Hammer, ItemSize.OneHanded, HipHolster);
+            _context.ItemStorage.Add(hammer);
+            hammer.HoldRightHand();
+
+            var stick = _context.CreateItem(Stick, ItemSize.OneHanded, BackHolster);
+            stick.Holsters.First().Equip();
+            stick.HoldLeftHand();
+
+            var crowbar = _context.CreateItem(Stick, ItemSize.OneHanded, HipHolster);
+            crowbar.Holsters.First().Equip();
+            _context.ItemStorage.Add(crowbar);
+
+            var info = _infoGenerator.GenerateCurrentInfo();
+
+            Assert.Equal(3, info.Items.Count);
+
+            var hammerInfo = Assert.Single(info.Items, x => x.ItemName == Hammer);
+            Assert.True(hammerInfo.IsInRightHand);
+            Assert.True(hammerInfo.IsInStorage);
+            Assert.False(hammerInfo.IsInLeftHand);
+            Assert.Empty(hammerInfo.HolsterName);
+
+            var stickInfo = Assert.Single(info.Items, x => x.ItemName == Stick);
+            Assert.True(stickInfo.IsInLeftHand);
+            Assert.Equal(BackHolster, stickInfo.HolsterName);
+            Assert.False(stickInfo.IsInRightHand);
+            Assert.False(stickInfo.IsInStorage);
+
+            var crowbarInfo = Assert.Single(info.Items, x => x.ItemName == Crowbar);
+            Assert.True(crowbarInfo.IsInStorage);
+            Assert.Equal(HipHolster, crowbarInfo.HolsterName);
+            Assert.False(crowbarInfo.IsInRightHand);
+            Assert.False(crowbarInfo.IsInLeftHand);
         }
     }
 }
