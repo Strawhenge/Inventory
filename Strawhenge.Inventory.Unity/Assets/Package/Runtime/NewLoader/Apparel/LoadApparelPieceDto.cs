b07b81494ac94ed5854eb61f58f97433821b1595
Matt Bristow <strawhengestudios@gmail.com>
2024-05-24 16:22:55 +0100

    added load data dtos

---
+++ Strawhenge.Inventory.Unity/Assets/Package/Runtime/NewLoader/Apparel/LoadApparelPieceDto.cs
@@ -0,0 +1,15 @@
+﻿using Strawhenge.Inventory.Unity.Apparel;
+using System;
+
+namespace Strawhenge.Inventory.Unity.NewLoader
+{
+    public class LoadApparelPieceDto : ILoadApparelPiece
+    {
+        public LoadApparelPieceDto(IApparelPieceData apparelPiece)
+        {
+            ApparelPiece = apparelPiece ?? throw new ArgumentNullException(nameof(apparelPiece));
+        }
+
+        public IApparelPieceData ApparelPiece { get; }
+    }
+}
