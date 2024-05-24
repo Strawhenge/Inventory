364e168438763ad55046de53ab63981968d89593
Matt Bristow <strawhengestudios@gmail.com>
2024-05-24 15:03:53 +0100

    added initial load inventory script

---
+++ Strawhenge.Inventory.Unity/Assets/Package/Runtime/NewLoader/LoadInventoryScript.cs
@@ -0,0 +1,16 @@
+﻿using Strawhenge.Inventory.Unity.Monobehaviours;
+using UnityEngine;
+
+namespace Strawhenge.Inventory.Unity.NewLoader
+{
+    [RequireComponent(typeof(InventoryScript))]
+    public class LoadInventoryScript : MonoBehaviour
+    {
+        InventoryScript _inventory;
+
+        void Awake()
+        {
+            _inventory = GetComponent<InventoryScript>();
+        }
+    }
+}
