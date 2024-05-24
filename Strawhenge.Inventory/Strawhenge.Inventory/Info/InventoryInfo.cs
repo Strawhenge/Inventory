0c9a8ec853e1cb9d0d22f09562576c1850327767
Matt Bristow <strawhengestudios@gmail.com>
2024-05-24 22:31:42 +0100

    added empty inventory info accessor

--- Strawhenge.Inventory/Strawhenge.Inventory/Info/InventoryInfo.cs
+++ Strawhenge.Inventory/Strawhenge.Inventory/Info/InventoryInfo.cs
@@ -1,10 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Strawhenge.Inventory.Info
 {
     public class InventoryInfo
     {
+        public static InventoryInfo Empty { get; } = new InventoryInfo(
+            items: Array.Empty<ItemInfo>(),
+            equippedApparel: Array.Empty<string>());
+
         public InventoryInfo(
             IEnumerable<ItemInfo> items,
             IEnumerable<string> equippedApparel)
