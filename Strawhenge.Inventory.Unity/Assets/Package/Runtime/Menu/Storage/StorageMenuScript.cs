1d58236986df2abd7d33c74d9e31f9b8ed0ddda0
Matt Bristow <strawhengestudios@gmail.com>
2024-06-02 14:00:46 +0100

    added weight display to menu

--- Strawhenge.Inventory.Unity/Assets/Package/Runtime/Menu/Storage/StorageMenuScript.cs
+++ Strawhenge.Inventory.Unity/Assets/Package/Runtime/Menu/Storage/StorageMenuScript.cs
@@ -1,23 +1,29 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Strawhenge.Inventory.Unity.Menu.Storage
 {
     public class StorageMenuScript : MonoBehaviour
     {
+        [SerializeField] Text _weightText;
         [SerializeField] StoredItemMenuEntryScript _menuEntryPrefab;
         [SerializeField] RectTransform _entryContainer;
 
         readonly Dictionary<IItem, StoredItemMenuEntryScript> _entries =
             new Dictionary<IItem, StoredItemMenuEntryScript>();
 
+        IStoredItems _storedItems;
+
         internal void Set(IStoredItems storedItems)
         {
-            foreach (var item in storedItems.Items)
+            _storedItems = storedItems;
+
+            foreach (var item in _storedItems.Items)
                 Add(item);
 
-            storedItems.ItemAdded += Add;
-            storedItems.ItemRemoved += Remove;
+            _storedItems.ItemAdded += Add;
+            _storedItems.ItemRemoved += Remove;
         }
 
         void Add(IItem item)
@@ -25,12 +31,19 @@ namespace Strawhenge.Inventory.Unity.Menu.Storage
             var entry = Instantiate(_menuEntryPrefab, parent: _entryContainer);
             entry.Set(item);
             _entries.Add(item, entry);
+
+            UpdateWeightText();
         }
 
         void Remove(IItem item)
         {
             Destroy(_entries[item].gameObject);
             _entries.Remove(item);
+
+            UpdateWeightText();
         }
+
+        void UpdateWeightText() =>
+            _weightText.text = $"{_storedItems.TotalItemsWeight} / {_storedItems.MaxItemsWeight}";
     }
 }
