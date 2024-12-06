8716eba6f10e89e6b67d24b26e50aed934bd4844
Matt Bristow <strawhengestudios@gmail.com>
2024-12-06 19:39:55 +0000

    Added item pickup script

---
+++ Strawhenge.Inventory.Unity/Assets/Package/Runtime/Item/ItemPickupScript.cs
@@ -0,0 +1,32 @@
+using Strawhenge.Inventory.Unity.Data;
+using Strawhenge.Inventory.Unity.Data.ScriptableObjects;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Strawhenge.Inventory.Unity.Items
+{
+    public class ItemPickupScript : MonoBehaviour
+    {
+        [SerializeField] ItemScriptableObject _data;
+        [SerializeField, Tooltip("Destroy the GameObject on pickup.")] bool _destroyOnPickup;
+        [SerializeField] UnityEvent _onPickup;
+        
+        void Awake()
+        {
+            if (_data == null)
+            {
+                Debug.LogError($"Missing {nameof(_data)}.", this);
+                Destroy(this);
+            }
+        }
+
+        internal IItemData PickupItem()
+        {
+            if (_destroyOnPickup)
+                Destroy(gameObject);
+
+            _onPickup.Invoke();
+            return _data;
+        }
+    }
+}
