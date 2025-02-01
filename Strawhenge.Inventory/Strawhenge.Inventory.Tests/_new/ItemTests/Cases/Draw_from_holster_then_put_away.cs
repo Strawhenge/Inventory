bcf3598d67d98806ea9f28fe54df652ca43edcdf
Strawhenge <strawhengestudios@gmail.com>
2025-02-01 09:51:55 +0000

    add test case

---
+++ Strawhenge.Inventory/Strawhenge.Inventory.Tests/_new/ItemTests/Cases/Draw_from_holster_then_put_away.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Strawhenge.Inventory.Items;
+using Xunit.Abstractions;
+
+namespace Strawhenge.Inventory.Tests._new
+{
+    public class Draw_from_holster_then_put_away : BaseItemTest
+    {
+        readonly Item _hammer;
+
+        public Draw_from_holster_then_put_away(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
+        {
+            AddHolster(RightHip);
+
+            _hammer = CreateItem(Hammer, new[] { RightHip });
+            _hammer.Holsters[RightHip].Do(x => x.Equip());
+            _hammer.HoldRightHand();
+            _hammer.PutAway();
+        }
+
+        protected override IEnumerable<(string holsterName, IItem expectedItem)> ExpectedItemsInHolsters()
+        {
+            yield return (RightHip, _hammer);
+        }
+
+        protected override IEnumerable<ViewCallInfo> ExpectedViewCalls()
+        {
+            yield return (Hammer, RightHip, x => x.Show);
+            yield return (Hammer, RightHip, x => x.DrawRightHand);
+            yield return (Hammer, RightHip, x => x.PutAwayRightHand);
+        }
+    }
+}
