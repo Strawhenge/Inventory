d5f73f70d5abe99b1c5ad9106a6db2d3cccd7977
Strawhenge <strawhengestudios@gmail.com>
2025-02-01 10:31:01 +0000

    add test case

---
+++ Strawhenge.Inventory/Strawhenge.Inventory.Tests/_new/ItemTests/Cases/Draw_one_handed_left_hand_when_two_handed_in_right_hand.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using FunctionalUtilities;
+using Strawhenge.Inventory.Items;
+using Xunit.Abstractions;
+
+namespace Strawhenge.Inventory.Tests._new
+{
+    public class Draw_one_handed_left_hand_when_two_handed_in_right_hand : BaseItemTest
+    {
+        readonly Item _spear;
+        readonly Item _hammer;
+
+        public Draw_one_handed_left_hand_when_two_handed_in_right_hand(ITestOutputHelper testOutputHelper) : base(
+            testOutputHelper)
+        {
+            _spear = CreateTwoHandedItem(Spear);
+            _spear.HoldRightHand();
+
+            _hammer = CreateItem(Hammer);
+            _hammer.HoldLeftHand();
+        }
+
+        protected override Maybe<Item> ExpectedItemInLeftHand => _hammer;
+
+        protected override IEnumerable<ViewCallInfo> ExpectedViewCalls()
+        {
+            yield return (Spear, x => x.DrawRightHand);
+            yield return (Spear, x => x.Disappear);
+
+            yield return (Hammer, x => x.DrawLeftHand);
+        }
+    }
+}
