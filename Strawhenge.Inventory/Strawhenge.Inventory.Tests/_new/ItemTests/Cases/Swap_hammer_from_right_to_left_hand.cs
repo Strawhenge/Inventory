20f4692bb3804c1415fc90aee51fdf76c9ae0c98
Strawhenge <strawhengestudios@gmail.com>
2025-02-01 09:37:53 +0000

    add test case

---
+++ Strawhenge.Inventory/Strawhenge.Inventory.Tests/_new/ItemTests/Cases/Swap_hammer_from_right_to_left_hand.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using FunctionalUtilities;
+using Strawhenge.Inventory.Items;
+using Xunit.Abstractions;
+
+namespace Strawhenge.Inventory.Tests._new
+{
+    public class Swap_hammer_from_right_to_left_hand : BaseItemTest
+    {
+        readonly Item _hammer;
+
+        public Swap_hammer_from_right_to_left_hand(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
+        {
+            _hammer = CreateItem(Hammer);
+            _hammer.HoldRightHand();
+            _hammer.HoldLeftHand();
+        }
+
+        protected override Maybe<Item> ExpectedItemInLeftHand => _hammer;
+
+        protected override IEnumerable<ViewCallInfo> ExpectedViewCalls()
+        {
+            yield return (Hammer, x => x.DrawRightHand);
+            yield return (Hammer, x => x.RightHandToLeftHand);
+        }
+    }
+}
