155a98347c45d0ecf1dfcb247689361b36211f3b
Strawhenge <strawhengestudios@gmail.com>
2025-02-04 08:59:51 +0000

    add test cases

---
+++ Strawhenge.Inventory/Strawhenge.Inventory.Tests/_new/ItemTests/Cases/Unequip_from_holster.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Strawhenge.Inventory.Items;
+using Xunit.Abstractions;
+
+namespace Strawhenge.Inventory.Tests._new
+{
+    public class Unequip_from_holster : BaseItemTest
+    {
+        readonly Item _hammer;
+
+        public Unequip_from_holster(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
+        {
+            AddHolster(RightHipHolster);
+
+            _hammer = CreateItem(Hammer, new[] { RightHipHolster });
+            _hammer.Holsters[RightHipHolster].Do(x => x.Equip());
+            _hammer.Holsters[RightHipHolster].Do(x => x.Unequip());
+        }
+
+        protected override IEnumerable<ViewCallInfo> ExpectedViewCalls()
+        {
+            yield return (Hammer, RightHipHolster, x => x.Show);
+            yield return (Hammer, RightHipHolster, x => x.Hide);
+        }
+    }
+}
