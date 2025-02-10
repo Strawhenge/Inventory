d49239a7547eaf05f761ae1801bdf86e07b74368
Strawhenge <strawhengestudios@gmail.com>
2025-02-10 12:05:23 +0000

    add test cases

---
+++ Strawhenge.Inventory/Strawhenge.Inventory.Tests/_new/ItemTests/Drop/Drop_from_hammerspace.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Strawhenge.Inventory.Items;
+using Xunit.Abstractions;
+
+namespace Strawhenge.Inventory.Tests._new.ItemTests.Drop
+{
+    public class Drop_from_hammerspace : BaseItemTest
+    {
+        readonly Item _hammer;
+
+        public Drop_from_hammerspace(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
+        {
+            _hammer = CreateItem(Hammer);
+            _hammer.Drop();
+        }
+
+        protected override IEnumerable<ViewCallInfo> ExpectedViewCalls()
+        {
+            yield return (Hammer, x => x.SpawnAndDrop);
+        }
+    }
+}
---
+++ Strawhenge.Inventory/Strawhenge.Inventory.Tests/_new/ItemTests/Drop/Drop_when_equipped_to_holster_and_in_hand.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Xunit.Abstractions;
+
+namespace Strawhenge.Inventory.Tests._new.ItemTests.Drop
+{
+    public class Drop_when_equipped_to_holster_and_in_hand : BaseItemTest
+    {
+        public Drop_when_equipped_to_holster_and_in_hand(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
+        {
+            AddHolster(RightHipHolster);
+
+            var hammer = CreateItem(Hammer, new[] { RightHipHolster });
+            hammer.Holsters[RightHipHolster].Do(x => x.Equip());
+            hammer.HoldRightHand();
+            hammer.Drop();
+        }
+
+        protected override IEnumerable<ViewCallInfo> ExpectedViewCalls()
+        {
+            yield return (Hammer, RightHipHolster, x => x.Show);
+            yield return (Hammer, RightHipHolster, x => x.DrawRightHand);
+
+            yield return (Hammer, x => x.DropRightHand);
+        }
+    }
+}
