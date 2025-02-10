fb27182cc1d866d1b8a1807f2b167c32be1976de
Strawhenge <strawhengestudios@gmail.com>
2025-02-10 13:27:14 +0000

    add test cases

---
+++ Strawhenge.Inventory/Strawhenge.Inventory.Tests/_new/ItemTests/ClearFromHands/Clear_stored_item_from_hands.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Strawhenge.Inventory.Items;
+using Xunit.Abstractions;
+
+namespace Strawhenge.Inventory.Tests._new.ItemTests.ClearFromHands
+{
+    public class Clear_stored_item_from_hands : BaseItemTest
+    {
+        readonly Item _hammer;
+
+        public Clear_stored_item_from_hands(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
+        {
+            SetStorageCapacity(10);
+
+            _hammer = CreateItem(Hammer, storable: true);
+            _hammer.HoldRightHand();
+            _hammer.Storable.Do(x => x.AddToStorage());
+            _hammer.ClearFromHands();
+        }
+
+        protected override IEnumerable<IItem> ExpectedItemsInStorage()
+        {
+            yield return _hammer;
+        }
+
+        protected override IEnumerable<ViewCallInfo> ExpectedViewCalls()
+        {
+            yield return (Hammer, x => x.DrawRightHand);
+            yield return (Hammer, x => x.PutAwayRightHand);
+        }
+    }
+}
