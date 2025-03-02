ff0d48b3796ed970c2e6c6405ca6ea2e29cbb994
Strawhenge <strawhengestudios@gmail.com>
2025-03-02 10:16:31 +0000

    added inventory toggle input to dev scene

---
+++ Strawhenge.Inventory.Unity/Assets/Development/Scripts/ToggleMenuScript.cs
@@ -0,0 +1,27 @@
+using Strawhenge.Common.Unity.Helpers;
+using Strawhenge.Inventory.Unity.Menu;
+using UnityEngine;
+
+public class ToggleMenuScript : MonoBehaviour
+{
+    [SerializeField] InventoryMenuScript _menu;
+
+    void Awake()
+    {
+        ComponentRefHelper.EnsureHierarchyComponent(ref _menu, nameof(_menu), this);
+
+        if (_menu == null)
+            enabled = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            if (_menu.IsOpen)
+                _menu.Close();
+            else
+                _menu.Open();
+        }
+    }
+}
