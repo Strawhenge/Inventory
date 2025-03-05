d3351458981f9c842d165e4c4ac004adf8647898
Strawhenge <strawhengestudios@gmail.com>
2025-03-05 04:11:14 +0000

    added lootbox to dev scene

---
+++ Strawhenge.Inventory.Unity/Assets/Development/Scripts/LootboxScript.cs
@@ -0,0 +1,33 @@
+using Strawhenge.Common.Unity.Helpers;
+using Strawhenge.Inventory.Unity;
+using UnityEngine;
+
+public class LootboxScript : MonoBehaviour
+{
+    [SerializeField] FixedItemContainerScript _itemContainer;
+    [SerializeField] ItemContainerMenuScript _itemContainerMenu;
+    [SerializeField] Camera _camera;
+
+    void Awake()
+    {
+        ComponentRefHelper.EnsureHierarchyComponent(ref _itemContainer, nameof(_itemContainer), this);
+        ComponentRefHelper.EnsureSceneComponent(ref _itemContainerMenu, nameof(_itemContainerMenu), this);
+        ComponentRefHelper.EnsureCamera(ref _camera, nameof(_camera), this);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            _itemContainerMenu.Close();
+
+        if (ClickedOnLootbox())
+            _itemContainerMenu.Open(_itemContainer.Source);
+    }
+
+    bool ClickedOnLootbox()
+    {
+        return Input.GetMouseButton(0) &&
+               Physics.Raycast(_camera.ScreenPointToRay(Input.mousePosition), out RaycastHit hit) &&
+               hit.transform.root == transform;
+    }
+}
