7a25971abec54acd37bf71f6d441f1e36dc4f5ec
Strawhenge <strawhengestudios@gmail.com>
2025-03-12 05:28:52 +0000

    initial implementation of item factory

---
+++ Strawhenge.Inventory/Strawhenge.Inventory/Item/ItemData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using FunctionalUtilities;
+
+namespace Strawhenge.Inventory.Items
+{
+    public class ItemData
+    {
+        public string Name { get; }
+
+        public ItemSize Size { get; }
+
+        public bool IsStorable { get; }
+
+        public int Weight { get; }
+
+        public IReadOnlyList<HolsterItemData> Holsters { get; }
+
+        public Maybe<T> Get<T>() where T : class => throw new NotImplementedException();
+    }
+
+    public class HolsterItemData
+    {
+        public string HolsterName { get; }
+
+        public Maybe<T> Get<T>() where T : class => throw new NotImplementedException();
+    }
+}
---
+++ Strawhenge.Inventory/Strawhenge.Inventory/Item/ItemFactory.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+using Strawhenge.Inventory.Containers;
+using Strawhenge.Inventory.Items.Holsters;
+
+namespace Strawhenge.Inventory.Items
+{
+    class ItemFactory
+    {
+        readonly Hands _hands;
+        readonly Containers.Holsters _holsters;
+        readonly StoredItems _storedItems;
+        readonly IItemViewFactory _itemViewFactory;
+        readonly IHolsterForItemViewFactory _holsterForItemViewFactory;
+
+        public ItemFactory(
+            Hands hands,
+            Containers.Holsters holsters,
+            StoredItems storedItems,
+            IItemViewFactory itemViewFactory,
+            IHolsterForItemViewFactory holsterForItemViewFactory)
+        {
+            _hands = hands;
+            _holsters = holsters;
+            _storedItems = storedItems;
+            _itemViewFactory = itemViewFactory;
+            _holsterForItemViewFactory = holsterForItemViewFactory;
+        }
+
+        public Item Create(ItemData data)
+        {
+            var view = _itemViewFactory.Create(data);
+            
+            var item = new Item(
+                data.Name,
+                _hands,
+                view,
+                data.Size
+            );
+
+            var holstersForItem = data.Holsters
+                .Select(holsterData =>
+                {
+                    return _holsters
+                        .FindByName(holsterData.HolsterName)
+                        .Map(container =>
+                        {
+                            var holsterForItemView = _holsterForItemViewFactory.Create(holsterData);
+                            return new HolsterForItem(item, container, holsterForItemView);
+                        });
+                })
+                .SelectMany(x => x.AsEnumerable());
+
+            item.SetupHolsters(new HolstersForItem(holstersForItem));
+
+            if (data.IsStorable)
+                item.SetupStorable(_storedItems, data.Weight);
+
+            return item;
+        }
+    }
+
+    public interface IItemViewFactory
+    {
+        IItemView Create(ItemData data);
+    }
+
+    public interface IHolsterForItemViewFactory
+    {
+        IHolsterForItemView Create(HolsterItemData data);
+    }
+}
