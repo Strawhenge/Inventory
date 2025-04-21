e46b0a5940604684f62308bf26075e4959c5bd8d
Strawhenge <strawhengestudios@gmail.com>
2025-04-21 08:41:37 +0100

    remove incorrect item factory method

--- Strawhenge.Inventory/Strawhenge.Inventory/Item/ItemFactory.cs
+++ Strawhenge.Inventory/Strawhenge.Inventory/Item/ItemFactory.cs
@@ -10,63 +10,20 @@ namespace Strawhenge.Inventory.Items
         readonly Hands _hands;
         readonly Containers.Holsters _holsters;
         readonly StoredItems _storedItems;
-        readonly IItemViewFactory _itemViewFactory;
-        readonly IHolsterForItemViewFactory _holsterForItemViewFactory;
 
         public ItemFactory(
             Hands hands,
             Containers.Holsters holsters,
-            StoredItems storedItems,
-            IItemViewFactory itemViewFactory,
-            IHolsterForItemViewFactory holsterForItemViewFactory)
+            StoredItems storedItems)
         {
             _hands = hands;
             _holsters = holsters;
             _storedItems = storedItems;
-            _itemViewFactory = itemViewFactory;
-            _holsterForItemViewFactory = holsterForItemViewFactory;
         }
 
         public Item Create(ItemData data)
         {
-            var view = _itemViewFactory.Create(data);
-            
-            var item = new Item(
-                data.Name,
-                _hands,
-                view,
-                data.Size
-            );
-
-            var holstersForItem = data.Holsters
-                .Select(holsterData =>
-                {
-                    return _holsters
-                        .FindByName(holsterData.HolsterName)
-                        .Map(container =>
-                        {
-                            var holsterForItemView = _holsterForItemViewFactory.Create(holsterData);
-                            return new HolsterForItem(item, container, holsterForItemView);
-                        });
-                })
-                .SelectMany(x => x.AsEnumerable());
-
-            item.SetupHolsters(new HolstersForItem(holstersForItem));
-
-            if (data.IsStorable)
-                item.SetupStorable(_storedItems, data.Weight);
-
-            return item;
+            throw new NotImplementedException();
         }
-    }
-
-    public interface IItemViewFactory
-    {
-        IItemView Create(ItemData data);
-    }
-
-    public interface IHolsterForItemViewFactory
-    {
-        IHolsterForItemView Create(HolsterItemData data);
     }
 }
