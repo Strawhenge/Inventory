a43a393182e82ec3e9edf667d3c8b7e17226be61
Strawhenge <strawhengestudios@gmail.com>
2025-04-24 06:00:15 +0100

    implemented ApparelViewFactory

---
+++ Strawhenge.Inventory.Unity/Assets/Package/Runtime/Apparel/ApparelViewFactory.cs
@@ -0,0 +1,55 @@
+﻿using Strawhenge.Common.Logging;
+using Strawhenge.Inventory.Apparel;
+
+namespace Strawhenge.Inventory.Unity.Apparel
+{
+    public class ApparelViewFactory : IApparelViewFactory
+    {
+        readonly ApparelSlotScripts _slotScripts;
+        readonly IApparelGameObjectInitializer _gameObjectInitializer;
+        readonly IApparelDrop _apparelDrop;
+        readonly IApparelLayerAccessor _layerAccessor;
+        readonly ILogger _logger;
+
+        public ApparelViewFactory(
+            ApparelSlotScripts slotScripts,
+            IApparelGameObjectInitializer gameObjectInitializer,
+            IApparelDrop apparelDrop,
+            IApparelLayerAccessor layerAccessor,
+            ILogger logger)
+        {
+            _slotScripts = slotScripts;
+            _gameObjectInitializer = gameObjectInitializer;
+            _apparelDrop = apparelDrop;
+            _layerAccessor = layerAccessor;
+            _logger = logger;
+        }
+
+        public IApparelView Create(ApparelPieceData data)
+        {
+            var apparelPieceData = data
+                .Get<IApparelPieceData>()
+                .Reduce(() =>
+                {
+                    // TODO
+                    throw new System.NotImplementedException();
+                });
+
+
+            if (!_slotScripts.FindByName(apparelPieceData.Slot).HasSome(out var slotScript))
+            {
+                _logger.LogWarning($"Missing apparel slot: '{apparelPieceData.Slot}'.");
+                return new NullApparelView();
+            }
+
+            var view = new ApparelView(
+                apparelPieceData,
+                _gameObjectInitializer,
+                _layerAccessor,
+                _apparelDrop,
+                slotScript.transform);
+
+            return view;
+        }
+    }
+}
