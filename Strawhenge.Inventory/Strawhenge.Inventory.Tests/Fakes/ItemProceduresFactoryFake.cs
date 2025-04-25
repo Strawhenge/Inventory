ba767c7d9a8dff7507ce9fb2f77b453fb321439f
Strawhenge <strawhengestudios@gmail.com>
2025-04-25 09:40:58 +0100

    item proc factory fake

---
+++ Strawhenge.Inventory/Strawhenge.Inventory.Tests/Fakes/ItemProceduresFactoryFake.cs
@@ -0,0 +1,30 @@
+﻿using Strawhenge.Inventory.Items;
+
+namespace Strawhenge.Inventory.Tests
+{
+    class ItemProceduresFactoryFake : IItemProceduresFactory
+    {
+        readonly ProcedureTracker _tracker;
+
+        public ItemProceduresFactoryFake(ProcedureTracker tracker)
+        {
+            _tracker = tracker;
+        }
+
+        public ItemProcedureDto Create(ItemData itemData)
+        {
+            var itemProcedures = new ItemProceduresFake(_tracker, itemData.Name);
+
+            var dto = new ItemProcedureDto(itemProcedures);
+
+            foreach (var holsterData in itemData.Holsters)
+            {
+                var holsterProcedures =
+                    new HolsterForItemProceduresFake(_tracker, itemData.Name, holsterData.HolsterName);
+                dto.SetHolster(holsterData.HolsterName, holsterProcedures);
+            }
+
+            return dto;
+        }
+    }
+}
