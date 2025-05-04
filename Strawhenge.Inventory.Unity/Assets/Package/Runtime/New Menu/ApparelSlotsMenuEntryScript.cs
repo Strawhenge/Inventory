9df692422d51c971f98628c2e304aaf0fb6073db
Strawhenge <strawhengestudios@gmail.com>
2025-05-04 10:53:36 +0100

    implemented apparel slots menu

--- Strawhenge.Inventory.Unity/Assets/Package/Runtime/New Menu/ApparelSlotsMenuEntryScript.cs
+++ Strawhenge.Inventory.Unity/Assets/Package/Runtime/New Menu/ApparelSlotsMenuEntryScript.cs
@@ -1,10 +1,44 @@
+using Strawhenge.Inventory.Apparel;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Strawhenge.Inventory.Unity
 {
     public class ApparelSlotsMenuEntryScript : MonoBehaviour
     {
+        [SerializeField] Text _apparelSlotNameText;
+        [SerializeField] Text _apparelPieceNameText;
+        [SerializeField] Button _unequipButton;
+
+        ApparelPiece _apparelPiece;
+
+        public void SetApparelSlot(ApparelSlot slot)
+        {
+            _apparelSlotNameText.text = slot.Name;
+            _apparelPieceNameText.text = string.Empty;
+            _unequipButton.interactable = false;
+
+            slot.Changed += () =>
+            {
+                if (slot.CurrentPiece.HasSome(out var piece))
+                {
+                    _apparelPiece = piece;
+                    _apparelPieceNameText.text = piece.Name;
+                    _unequipButton.interactable = true;
+                }
+                else
+                {
+                    _apparelPiece = null;
+                    _apparelPieceNameText.text = string.Empty;
+                    _unequipButton.interactable = false;
+                }
+            };
+
+            _unequipButton.onClick.AddListener(Unequip);
+        }
+
+        void Unequip() => _apparelPiece?.Unequip();
     }
 }
--- Strawhenge.Inventory.Unity/Assets/Package/Runtime/New Menu/ApparelSlotsMenuScript.cs
+++ Strawhenge.Inventory.Unity/Assets/Package/Runtime/New Menu/ApparelSlotsMenuScript.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Strawhenge.Inventory.Unity.NewMenu
 {
     public class ApparelSlotsMenuScript : MonoBehaviour
     {
         [SerializeField] ApparelSlotsMenuEntryScript _menuEntryPrefab;
+        [SerializeField] RectTransform _menuEntriesParent;
 
         public void SetInventory(Inventory inventory)
         {
+            foreach (var slot in inventory.ApparelSlots)
+            {
+                var menuEntry = Instantiate(_menuEntryPrefab, _menuEntriesParent);
+                menuEntry.SetApparelSlot(slot);
+            }
         }
     }
 }
