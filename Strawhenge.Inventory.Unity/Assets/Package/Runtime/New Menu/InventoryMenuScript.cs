8a4cd16c9247a80a80be6b5f8bbbfa086906a26f
Strawhenge <strawhengestudios@gmail.com>
2025-05-04 09:43:14 +0100

    implemented submenu switch

--- Strawhenge.Inventory.Unity/Assets/Package/Runtime/New Menu/InventoryMenuScript.cs
+++ Strawhenge.Inventory.Unity/Assets/Package/Runtime/New Menu/InventoryMenuScript.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Strawhenge.Inventory.Unity.NewMenu
 {
@@ -7,6 +8,10 @@ namespace Strawhenge.Inventory.Unity.NewMenu
         [SerializeField] InventoryScript _player;
         [SerializeField] HandsMenuScript _handsMenu;
         [SerializeField] HolstersMenuScript _holstersMenu;
+        [SerializeField] Button _handsMenuButton;
+        [SerializeField] Button _holstersMenuButton;
+        [SerializeField] Button _storageMenuButton;
+        [SerializeField] Button _apparelMenuButton;
 
         void Start()
         {
@@ -17,6 +22,57 @@ namespace Strawhenge.Inventory.Unity.NewMenu
                     _handsMenu.SetInventory(_player.Inventory);
                     _holstersMenu.SetInventory(_player.Inventory);
                 });
+
+            _handsMenuButton.onClick.AddListener(SelectHandsMenu);
+            _holstersMenuButton.onClick.AddListener(SelectHolstersMenu);
+            _storageMenuButton.onClick.AddListener(SelectStorageMenu);
+            _apparelMenuButton.onClick.AddListener(SelectApparelMenu);
+
+            SelectHandsMenu();
+        }
+
+        void SelectHandsMenu()
+        {
+            _handsMenu.gameObject.SetActive(true);
+            _holstersMenu.gameObject.SetActive(false);
+
+            _handsMenuButton.interactable = false;
+            _holstersMenuButton.interactable = true;
+            _storageMenuButton.interactable = true;
+            _apparelMenuButton.interactable = true;
+        }
+
+        void SelectHolstersMenu()
+        {
+            _handsMenu.gameObject.SetActive(false);
+            _holstersMenu.gameObject.SetActive(true);
+
+            _handsMenuButton.interactable = true;
+            _holstersMenuButton.interactable = false;
+            _storageMenuButton.interactable = true;
+            _apparelMenuButton.interactable = true;
+        }
+
+        void SelectStorageMenu()
+        {
+            _handsMenu.gameObject.SetActive(false);
+            _holstersMenu.gameObject.SetActive(false);
+
+            _handsMenuButton.interactable = true;
+            _holstersMenuButton.interactable = true;
+            _storageMenuButton.interactable = false;
+            _apparelMenuButton.interactable = true;
+        }
+
+        void SelectApparelMenu()
+        {
+            _handsMenu.gameObject.SetActive(false);
+            _holstersMenu.gameObject.SetActive(false);
+
+            _handsMenuButton.interactable = true;
+            _holstersMenuButton.interactable = true;
+            _storageMenuButton.interactable = true;
+            _apparelMenuButton.interactable = false;
         }
     }
 }
