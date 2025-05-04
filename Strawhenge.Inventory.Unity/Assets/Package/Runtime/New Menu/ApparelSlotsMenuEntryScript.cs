0f87b0fd731d5b8c3fa6a05dca128bdb4311ebfa
Strawhenge <strawhengestudios@gmail.com>
2025-05-04 10:54:18 +0100

    tidy up

--- Strawhenge.Inventory.Unity/Assets/Package/Runtime/New Menu/ApparelSlotsMenuEntryScript.cs
+++ Strawhenge.Inventory.Unity/Assets/Package/Runtime/New Menu/ApparelSlotsMenuEntryScript.cs
@@ -1,6 +1,4 @@
 using Strawhenge.Inventory.Apparel;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
--- Strawhenge.Inventory.Unity/Assets/Package/Runtime/New Menu/StoredItemsMenuEntryScript.cs
+++ Strawhenge.Inventory.Unity/Assets/Package/Runtime/New Menu/StoredItemsMenuEntryScript.cs
@@ -1,7 +1,5 @@
 using Strawhenge.Inventory.Items;
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
