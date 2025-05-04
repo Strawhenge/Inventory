c553e7b2f2c622bb7b80cf7b2ad03647414b5075
Strawhenge <strawhengestudios@gmail.com>
2025-05-04 10:14:27 +0100

    implmented stored items menu

---
+++ Strawhenge.Inventory.Unity/Assets/Package/Runtime/New Menu/StoredItemsMenuEntryScript.cs
@@ -0,0 +1,23 @@
+using Strawhenge.Inventory.Items;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Strawhenge.Inventory.Unity
+{
+    public class StoredItemsMenuEntryScript : MonoBehaviour
+    {
+        [SerializeField] Button _itemSelectButton;
+        [SerializeField] Text _itemNameText;
+
+        public event Action Selected;
+
+        public void SetItem(Item item)
+        {
+            _itemNameText.text = item.Name;
+            _itemSelectButton.onClick.AddListener(() => Selected?.Invoke());
+        }
+    }
+}
--- Strawhenge.Inventory.Unity/Assets/Package/Runtime/New Menu/StoredItemsMenuScript.cs
+++ Strawhenge.Inventory.Unity/Assets/Package/Runtime/New Menu/StoredItemsMenuScript.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+using Strawhenge.Inventory.Items;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,10 +7,36 @@ namespace Strawhenge.Inventory.Unity.NewMenu
     public class StoredItemsMenuScript : MonoBehaviour
     {
         [SerializeField] ItemMenuScript _itemMenuScript;
-        
+        [SerializeField] StoredItemsMenuEntryScript _menuEntryPrefab;
+        [SerializeField] RectTransform _menuEntriesParent;
+
+        readonly Dictionary<Item, StoredItemsMenuEntryScript> _menuEntriesByItem = new();
+
+        Item _selectedItem;
+
         public void SetInventory(Inventory inventory)
         {
-            
+            inventory.StoredItems.ItemAdded += item =>
+            {
+                _selectedItem = item;
+
+                var menuEntry = Instantiate(_menuEntryPrefab, _menuEntriesParent);
+                menuEntry.SetItem(item);
+                menuEntry.Selected += () => _itemMenuScript.SetItem(item);
+
+                _menuEntriesByItem.Add(item, menuEntry);
+            };
+
+            inventory.StoredItems.ItemRemoved += item =>
+            {
+                var menuEntry = _menuEntriesByItem[item];
+                _menuEntriesByItem.Remove(item);
+                
+                Destroy(menuEntry.gameObject);
+
+                if (_selectedItem == item)
+                    _itemMenuScript.UnsetItem();
+            };
         }
     }
 }
