7395935864e8483db36ba9962ee5be977b97becd
Strawhenge <strawhengestudios@gmail.com>
2025-05-04 06:36:17 +0100

    intial menu prefabs

--- Strawhenge.Inventory.Unity/Assets/Package/Runtime/New Menu/ItemMenuScript.cs
+++ Strawhenge.Inventory.Unity/Assets/Package/Runtime/New Menu/ItemMenuScript.cs
@@ -2,21 +2,21 @@ using Strawhenge.Inventory.Items;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Strawhenge.Inventory.Unity
 {
     public class ItemMenuScript : MonoBehaviour
     {
-       
+        [SerializeField] Text _itemNameText;
 
         public void SetItem(Item item)
         {
-            
+            _itemNameText.text = item.Name;
         }
 
         public void UnsetItem()
         {
-         
         }
     }
 }
