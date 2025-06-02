fedfdb79d71f52b7a456669892f98f509d0879a5
Strawhenge <strawhengestudios@gmail.com>
2025-06-02 14:10:14 +0100

    implemented inventory state importer and exporter

---
+++ Strawhenge.Inventory/Strawhenge.Inventory/ImportExport/InventoryStateExporter.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FunctionalUtilities;
+using Strawhenge.Inventory.Items;
+
+namespace Strawhenge.Inventory.ImportExport
+{
+    class InventoryStateExporter
+    {
+        readonly Inventory _inventory;
+
+        public InventoryStateExporter(Inventory inventory)
+        {
+            _inventory = inventory;
+        }
+
+        public InventoryState Export()
+        {
+            return new InventoryState(
+                ExportItems(), 
+                ExportApparelPieces());
+        }
+
+        IEnumerable<ItemState> ExportItems()
+        {
+            return _inventory.StoredItems.Items
+                .Concat(_inventory.Holsters.SelectMany(x => x.CurrentItem.AsEnumerable()))
+                .Concat(_inventory.Hands.LeftHand.CurrentItem.AsEnumerable())
+                .Concat(_inventory.Hands.RightHand.CurrentItem.AsEnumerable())
+                .Distinct()
+                .Select(item =>
+                {
+                    var inHand = _inventory.Hands.LeftHand.IsCurrentItem(item)
+                        ? ItemInHandState.InLeftHand
+                        : _inventory.Hands.RightHand.IsCurrentItem(item)
+                            ? ItemInHandState.InRightHand
+                            : ItemInHandState.NotInHands;
+
+                    var holster = item.Holsters.IsEquippedToHolster(out InventoryItemHolster itemHolster)
+                        ? itemHolster.HolsterName
+                        : null;
+
+                    return new ItemState(item.Item, item.Context, inHand, holster, item.IsInStorage);
+                });
+        }
+
+        IEnumerable<ApparelPieceState> ExportApparelPieces()
+        {
+            return _inventory.ApparelSlots
+                .SelectMany(slot => slot.CurrentPiece.AsEnumerable())
+                .Select(apparelPiece => new ApparelPieceState(apparelPiece.Piece));
+        }
+    }
+}
---
+++ Strawhenge.Inventory/Strawhenge.Inventory/ImportExport/ItemState.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using FunctionalUtilities;
+using Strawhenge.Inventory.Items;
+
+namespace Strawhenge.Inventory.ImportExport
+{
+    public class ItemState
+    {
+        public ItemState(
+            Item item,
+            Context context,
+            ItemInHandState inHand = ItemInHandState.NotInHands,
+            string holster = null,
+            bool isInStorage = false)
+        {
+            Item = item ?? throw new ArgumentNullException(nameof(item));
+            Context = context ?? throw new ArgumentNullException(nameof(context));
+
+            InHand = inHand;
+            Holster = holster ?? Maybe.None<string>();
+            IsInStorage = isInStorage;
+        }
+
+        public Item Item { get; }
+
+        public Context Context { get; }
+
+        public ItemInHandState InHand { get; }
+
+        public Maybe<string> Holster { get; }
+
+        public bool IsInStorage { get; }
+    }
+}
