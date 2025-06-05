582ba9225e0cba8dae2d5547ee96c357a1c4cfb7
Strawhenge <strawhengestudios@gmail.com>
2025-06-05 09:23:21 +0100

    dev scene

--- Strawhenge.Inventory.Unity/Assets/Development/Scripts/ContextScript.cs
+++ Strawhenge.Inventory.Unity/Assets/Development/Scripts/ContextScript.cs
@@ -1,14 +1,57 @@
-using System.Collections;
-using System.Collections.Generic;
+using Strawhenge.Common.Unity.Helpers;
+using Strawhenge.Inventory.Unity;
+using Strawhenge.Inventory.Unity.Loot;
+using Strawhenge.Inventory.Unity.Menu;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ContextScript : MonoBehaviour
 {
+    [SerializeField] InventoryMenuScript _inventoryMenu;
+    [SerializeField] LootMenuScript _lootMenu;
+    [SerializeField] Camera _camera;
+
+    void Awake()
+    {
+        ComponentRefHelper.EnsureSceneComponent(ref _inventoryMenu, nameof(_inventoryMenu), this);
+        ComponentRefHelper.EnsureSceneComponent(ref _lootMenu, nameof(_lootMenu), this);
+        ComponentRefHelper.EnsureCamera(ref _camera, nameof(_camera), this);
+    }
+
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            if (_inventoryMenu.IsOpen)
+                _inventoryMenu.Close();
+            else
+                _inventoryMenu.Open();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape) && _lootMenu.IsOpen)
+            _lootMenu.Close();
+
+        if (Input.GetMouseButton(1))
+            TryLoot();
+
         if (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.R))
             ReloadScene();
+    }
+
+    void TryLoot()
+    {
+        if (!Input.GetMouseButton(1) ||
+            !Physics.Raycast(_camera.ScreenPointToRay(Input.mousePosition), out RaycastHit hit))
+            return;
+
+        if (hit.transform.TryGetComponent<InventoryScript>(out var inventory))
+        {
+            _lootMenu.Open(inventory.Inventory.Loot());
+            return;
+        }
+
+        if (hit.transform.TryGetComponent<LootCollectionScript>(out var lootCollection))
+            _lootMenu.Open(lootCollection.Source);
     }
 
     [ContextMenu(nameof(ReloadScene))]
--- Strawhenge.Inventory.Unity/Assets/Development/Scripts/LootboxScript.cs
+++ Strawhenge.Inventory.Unity/Assets/Development/Scripts/LootboxScript.cs
@@ -6,33 +6,6 @@ using UnityEngine;
 
 public class LootboxScript : MonoBehaviour
 {
-    [SerializeField] LootCollectionScript _itemContainer;
-    [SerializeField] LootMenuScript _itemContainerMenu;
-    [SerializeField] Camera _camera;
-
-    void Awake()
-    {
-        ComponentRefHelper.EnsureHierarchyComponent(ref _itemContainer, nameof(_itemContainer), this);
-        ComponentRefHelper.EnsureSceneComponent(ref _itemContainerMenu, nameof(_itemContainerMenu), this);
-        ComponentRefHelper.EnsureCamera(ref _camera, nameof(_camera), this);
-    }
-
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
-            _itemContainerMenu.Close();
-
-        if (ClickedOnLootbox())
-            _itemContainerMenu.Open(_itemContainer.Source);
-    }
-
-    bool ClickedOnLootbox()
-    {
-        return Input.GetMouseButton(1) &&
-               Physics.Raycast(_camera.ScreenPointToRay(Input.mousePosition), out RaycastHit hit) &&
-               hit.transform.root == transform;
-    }
-
     public void OnStateChanged(ILootCollectionInfo info)
     {
         if (info.Count == 0)
