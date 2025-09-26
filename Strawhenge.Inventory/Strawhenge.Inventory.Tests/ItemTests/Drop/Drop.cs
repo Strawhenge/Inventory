6e8e04c6a85748c095e3050beabfb74307cb3233
Strawhenge <strawhengestudios@gmail.com>
2025-09-26 20:40:09 +0100

    added extra test

---
+++ Strawhenge.Inventory/Strawhenge.Inventory.Tests/ItemTests/Drop/Drop.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Xunit.Abstractions;
+
+namespace Strawhenge.Inventory.Tests.ItemTests.Drop
+{
+    public class Drop : BaseInventoryItemTest
+    {
+        public Drop(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
+        {
+            var hammer = CreateItem(Hammer, storable: true);
+            hammer.Drop(Callback);
+        }
+
+        protected override IEnumerable<ProcedureInfo> ExpectedProceduresCompleted()
+        {
+            yield return (Hammer, SpawnAndDrop);
+        }
+    }
+}
