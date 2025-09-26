f2082e2ebeeaa721d9eb9cfca7c28a17118787db
Strawhenge <strawhengestudios@gmail.com>
2025-09-26 20:33:37 +0100

    added extra test

---
+++ Strawhenge.Inventory/Strawhenge.Inventory.Tests/ItemTests/HoldRightHand/Hold.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using FunctionalUtilities;
+using Strawhenge.Inventory.Items;
+using Xunit.Abstractions;
+
+namespace Strawhenge.Inventory.Tests.ItemTests.HoldRightHand
+{
+    public class Hold : BaseInventoryItemTest
+    {
+        readonly InventoryItem _hammer;
+
+        public Hold(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
+        {
+            SetStorageCapacity(100);
+
+            _hammer = CreateItem(Hammer, storable: true);
+            _hammer.HoldRightHand(Callback);
+        }
+
+        protected override Maybe<InventoryItem> ExpectedItemInRightHand => _hammer;
+
+        protected override IEnumerable<ProcedureInfo> ExpectedProceduresCompleted()
+        {
+            yield return (Hammer, AppearRightHand);
+        }
+    }
+}
