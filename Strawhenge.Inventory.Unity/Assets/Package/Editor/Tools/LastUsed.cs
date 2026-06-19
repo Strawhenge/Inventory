964397159c691e8ffdc19203f770353b7023d69d
Strawhenge <strawhengestudios@gmail.com>
2026-06-19 11:06:55 +1000

    deleted obsolete classes

--- Strawhenge.Inventory.Unity/Assets/Package/Editor/Tools/LastUsed.cs
+++
@@ -1,9 +0,0 @@
-using UnityEditor.Animations;
-
-namespace Strawhenge.Inventory.Unity.Editor.Tools
-{
-    static class LastUsed
-    {
-        public static AnimatorController AnimatorController { get; set; }
-    }
-}
